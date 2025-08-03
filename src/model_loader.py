7bf8ab645d5f28ff0043d7f66e1ef1692358ccce
irakozej <jeaniakoze68@gmail.com>
2025-08-03 22:50:02 +0200

    modified src/model_loader.py to dowload the model instantly

--- src/model_loader.py
+++ src/model_loader.py
@@ -1,17 +1,34 @@
 # src/model_loader.py
 
-from tensorflow.keras.models import load_model
+import os
+import tensorflow as tf
+import requests
 
 MODEL_PATH = 'models/mnist_model.h5'
+MODEL_URL = "https://drive.google.com/uc?export=download&id=1kJhLmabiVo8kBvEJpbyeejR034CAPM3i"
 
-# Global model variable
-model = load_model(MODEL_PATH)
+model = None
+
+def download_model():
+    if not os.path.exists("models"):
+        os.makedirs("models")
+    if not os.path.exists(MODEL_PATH):
+        print("🔄 Downloading model from Google Drive...")
+        response = requests.get(MODEL_URL)
+        with open(MODEL_PATH, "wb") as f:
+            f.write(response.content)
+        print("✅ Model downloaded.")
 
 def get_model():
     global model
+    if model is None:
+        download_model()
+        print("📦 Loading model...")
+        model = tf.keras.models.load_model(MODEL_PATH)
     return model
 
 def reload_model():
     global model
-    model = load_model(MODEL_PATH)
-    return model
+    download_model()
+    print("♻️ Reloading model...")
+    model = tf.keras.models.load_model(MODEL_PATH)
